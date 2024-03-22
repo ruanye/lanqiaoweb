64b3c0d4b9d21902546635ad586ad5c706d06a71
ruanye <ruanye@88.com>
2024-03-22 21:01:36 +0800

    课件提交

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -30,8 +30,15 @@ export default defineConfig({
         text: '师资培训课件',
         
         items: [
-          { text: '师资培训课件 （1）', link: '/03' },
-          { text: '备赛专项练习题', link: '/04' },
+          { text: '师资培训课件（HTML+CSS）', link: '/03' },
+          { text: '师资培训课件（函数封装）', link: '/04' },
+          { text: '师资培训课件 （DOM 操作）', link: '/05' },
+          { text: '师资培训课件 （ES6 题目）', link: '/06' },
+          { text: '师资培训课件 （Element Plus 题目）', link: '/07' },
+          { text: '师资培训课件 （Echarts 题目）', link: '/08' },
+          { text: '师资培训课件 （Vue3 题目）', link: '/09' },
+          { text: '师资培训课件 （node 题目）', link: '/10' },
+          { text: '备赛专项练习题', link: '/11' },
         ]
       }
     ],
